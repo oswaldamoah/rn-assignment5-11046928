1b0de44f543c103ee28461ad3da5ec02d00c2062
oswaldamoah <oswaldamoah00@gmail.com>
2024-06-26 02:19:25 +0000

    Added circular wrapper to action and transaction images. Also changed the border radius of the Card component

--- my-app/screens/HomeScreen.js
+++ my-app/screens/HomeScreen.js
@@ -33,22 +33,30 @@ const HomeScreen = () => {
 
         <ButtonRow>
           <ActionButton>
-            <Ionicons name="arrow-up" size={24} color={theme.iconColor} />
+            <CircleWrapper>
+              <Ionicons name="arrow-up" size={24} color={theme.iconColor} />
+            </CircleWrapper>
             <ButtonText><Text>Send</Text></ButtonText>
           </ActionButton>
 
           <ActionButton>
-            <Ionicons name="arrow-down" size={24} color={theme.iconColor} />
+            <CircleWrapper>
+              <Ionicons name="arrow-down" size={24} color={theme.iconColor} />
+            </CircleWrapper>
             <ButtonText><Text>Receive</Text></ButtonText> 
           </ActionButton>
             
           <ActionButton>
-            <FontAwesome6 name="dollar" size={24} color={theme.iconColor} />
+            <CircleWrapper>
+              <FontAwesome6 name="dollar" size={24} color={theme.iconColor} />
+            </CircleWrapper>
             <ButtonText><Text>Loan</Text></ButtonText> 
           </ActionButton>
 
           <ActionButton>
-            <FontAwesome6 name="add" size={24} color={theme.iconColor} />
+            <CircleWrapper>
+              <FontAwesome6 name="add" size={24} color={theme.iconColor} />
+            </CircleWrapper>
             <ButtonText><Text>Topup</Text></ButtonText> 
           </ActionButton>
 
@@ -57,52 +65,60 @@ const HomeScreen = () => {
         <Transactions>
           <TouchableOpacity>
             <TransactionItem>
-              <TransactionIcon source={Apple} />
+              <CircleWrapper>
+                <TransactionIcon source={Apple} />
+              </CircleWrapper>
               <TransactionInfo>
                 <TransactionTitle><Text>Apple Store</Text></TransactionTitle>
                 <TransactionCategory><Text>Entertainment</Text></TransactionCategory>
               </TransactionInfo>
               <TransactionDetails>
-                <TransactionAmount positive><Text>+$300</Text></TransactionAmount>
+                <TransactionAmount negative><Text>-$5.99</Text></TransactionAmount>
                 <TransactionDate><Text>12/06/2024</Text></TransactionDate>
               </TransactionDetails>
             </TransactionItem>
           </TouchableOpacity>
           <TouchableOpacity>
             <TransactionItem>
-              <TransactionIcon source={Spotify} />
+              <CircleWrapper>
+                <TransactionIcon source={Spotify} />
+              </CircleWrapper>
               <TransactionInfo>
                 <TransactionTitle><Text>Spotify</Text></TransactionTitle>
                 <TransactionCategory><Text>Music</Text></TransactionCategory>
               </TransactionInfo>
               <TransactionDetails>
-                <TransactionAmount negative><Text>-$88</Text></TransactionAmount>
+                <TransactionAmount negative><Text>-$12.99</Text></TransactionAmount>
                 <TransactionDate><Text>13/06/2024</Text></TransactionDate>
               </TransactionDetails>
             </TransactionItem>
           </TouchableOpacity>
           <TouchableOpacity>
             <TransactionItem>
-              <TransactionIcon source={moneyTransfer} />
+              <CircleWrapper>
+                <TransactionIcon source={moneyTransfer} />
+              </CircleWrapper>
               <TransactionInfo>
                 <TransactionTitle><Text>Money Transfer</Text></TransactionTitle>
                 <TransactionCategory><Text>Transaction</Text></TransactionCategory>
               </TransactionInfo>
               <TransactionDetails>
-                <TransactionAmount negative><Text>-$5.99</Text></TransactionAmount>
+                <TransactionAmount positive><Text>+$300</Text></TransactionAmount>
                 <TransactionDate><Text>14/06/2024</Text></TransactionDate>
               </TransactionDetails>
             </TransactionItem>
           </TouchableOpacity>
           <TouchableOpacity>
             <TransactionItem>
-              <TransactionIcon source={Grocery} />
+              <CircleWrapper>
+                <TransactionIcon source={Grocery} />
+              </CircleWrapper>
               <TransactionInfo>
                 <TransactionTitle><Text>Groceries</Text></TransactionTitle>
                 <TransactionCategory><Text>Shopping</Text></TransactionCategory>
               </TransactionInfo>
               <TransactionDetails>
-                <TransactionAmount negative><Text>-$12.99</Text></TransactionAmount>
+                <TransactionAmount negative><Text>-$88</Text></TransactionAmount>
                 <TransactionDate><Text>15/06/2024</Text></TransactionDate>
               </TransactionDetails>
             </TransactionItem>
@@ -114,6 +130,7 @@ const HomeScreen = () => {
 };
 
 // Styled Components remain the same as in your original code
+
 const Container = styled.View({
   padding: 20,
   backgroundColor: props => props.theme.background,
@@ -147,7 +164,7 @@ const Card = styled.View({
   marginBottom: 20,
   shadowColor: '#000',
   shadowOffset: {
-    width: 0,
+    width: 8,
     height: 2,
   },
   shadowOpacity: 0.25,
@@ -157,8 +174,8 @@ const Card = styled.View({
 
 const CardImage = styled.Image({
   width: '100%',
-  height: 270,
-  borderRadius: 10,
+  height: 200,
+  borderRadius: 20,
   resizeMode: 'center',
 });
 
@@ -170,20 +187,31 @@ const ButtonRow = styled.View({
   marginRight: 20,
 });
 
+
 const ActionButton = styled(TouchableOpacity)({
   alignItems: 'center',
   justifyContent: 'center',
-  width: 60,
-  height: 60,
-  borderRadius: 30,
+  width: 70, // Increased width to accommodate the text under the icon
+  height: 100, // Increased height to accommodate the text under the icon
+  borderRadius: 35, // Increased border radius to keep the circle shape
   marginBottom: 10,
+});
+
+const CircleWrapper = styled.View({
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: 50,
+  height: 50,
+  borderRadius: 25,
+  backgroundColor: 'rgba(128, 128, 128, 0.3)', // Grey with 30% opacity
+  marginBottom: 5, // Margin to create space between the icon and the text
 });
 
 const ButtonText = styled.Text({
   fontSize: 12,
-  marginTop: 5,
   textAlign: 'center',
   color: props => props.theme.text,
+  marginTop: 5, // Add some space above the text
 });
 
 const Transactions = styled.View({
@@ -200,13 +228,13 @@ const TransactionItem = styled.View({
 });
 
 const TransactionIcon = styled.Image({
-  width: 30,
-  height: 30,
-  marginRight: 10,
+  width: 24, // Size of the image should be less than CircleWrapper to center it
+  height: 24,
 });
 
 const TransactionInfo = styled.View({
   flex: 1,
+  paddingLeft: 20, // Increased padding to move text further away from the icon
 });
 
 const TransactionTitle = styled.Text({
